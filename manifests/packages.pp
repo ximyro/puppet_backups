ea7552141cbc56bf7bf4ea200545909d8aa10182
ximyro <ximyro@gmail.com>
2015-07-23 14:20:26 +0300

    move to bitbucket

--- manifests/packages.pp
+++
@@ -1,5 +0,0 @@
-class install_packages{
-package {
-    ['duplicity', 'python-boto', 'gnupg','gzip','tar','python-gobject']: ensure => present
-  }
-}
--- manifests/schedules.pp
+++
@@ -1,7 +0,0 @@
-class schedules{
-  schedule { 'maint':
-   range  => "2 - 4",
-   period => daily,
-   repeat => 1,
-  }
-}
